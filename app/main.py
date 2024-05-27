994c8740ef19c35d1aa036e79861e5bb1b451647
Новогран Валерия Игоревна <valeria.novogran@becloud.by>
2024-05-27 17:01:03 +0300

    Build: add script

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+url = 'https://ipapi.is/vpn-exit-nodes.html'
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+
+def find_row(soup):
+    data_item = []
+    all_name_fields = soup.select('table.is-striped > tbody > tr')
+    for data in all_name_fields:
+        cells = data.select('td')
+        texts = [cell.get_text().strip() for cell in cells]
+        data_item.append(texts)
+    return data_item
+
+
+data = find_row(soup)
+
+with open('output.csv', 'w', newline='', encoding='utf-8') as file:
+    header = ["VPN Service", "IP Address", "Organization", "City", "Country"]
+    writer = csv.writer(file)
+    writer.writerow(header)
+    for row in data:
+        writer.writerow(row)
